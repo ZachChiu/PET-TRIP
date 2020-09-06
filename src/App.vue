8636591e334d9966d0bf2ec0b647ed5f56dd87ce
Zach <awdx8326@gmail.com>
2020-09-06 18:15:21 +0800

    調整 App.vue 結構

--- src/App.vue
+++ src/App.vue
@@ -5,9 +5,29 @@
 </template>
 
 <style lang="scss">
+@import "./assets/scss/main";
+
 #app {
   font-family: -apple-system, BlinkMacSystemFont, "Noto Sans TC",
     Microsoft JhengHei, "Segoe UI", "Roboto", "Helvetica Neue", Arial,
     sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol";
+  h1 {
+    font-family: "Patrick Hand", cursive;
+  }
+  .headerNav {
+    top: 0;
+    z-index: 10;
+    box-shadow: 1px 1px 5px grey;
+  }
+  .bannerText {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background: rgba($color: #e7e7e7, $alpha: 0.3);
+    top: 0;
+    bottom: 0;
+    right: 0;
+    left: 0;
+  }
 }
 </style>
