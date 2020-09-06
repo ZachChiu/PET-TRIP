4c62448e0f19e2d6d36596b5c185a8483b7384ef
Zach <awdx8326@gmail.com>
2020-09-06 18:17:38 +0800

    新增 FirmRegister.vue 的 HTML 與 JS 結構

--- src/views/FirmRegister.vue
+++ src/views/FirmRegister.vue
@@ -1,5 +1,144 @@
 <template>
   <div class="firmRegister">
-    <h2>廠商註冊</h2>
+    <div class="banner position-relative">
+      <div class="bannerText position-absolute text-dark display-4 font-weight-bold">註冊</div>
+      <img
+        class="img-fluid"
+        src="https://upload.cc/i1/2020/09/02/JE5b96.png"
+        style="height: 200px;object-fit: cover; width: 100%;"
+        alt
+      />
+    </div>
+    <div class="container py-5">
+      <div class="row">
+        <div class="col-lg-6 col-11 mx-auto border rounded">
+          <form class="py-5 px-2 px-sm-5">
+            <h3 class="text-center mb-3">廠商註冊</h3>
+            <div class="form-group">
+              <label for="firmName">廠商名稱</label>
+              <input
+                type="text"
+                id="firmName"
+                class="form-control"
+                required
+                v-model="register.companyname"
+              />
+            </div>
+            <div class="form-group">
+              <label for="brandName">品牌名稱</label>
+              <input
+                type="text"
+                id="brandName"
+                class="form-control"
+                v-model="register.companybrand"
+              />
+            </div>
+            <div class="form-group">
+              <label for="phone">電話</label>
+              <input type="text" id="phone" class="form-control" v-model="register.phone" />
+            </div>
+            <div class="form-group">
+              <label for="email">信箱(帳號)</label>
+              <input type="email" class="form-control" id="email" v-model="register.email" />
+            </div>
+            <div class="form-group">
+              <label for="password">密碼</label>
+              <input type="password" class="form-control" id="password" v-model="register.pwd" />
+            </div>
+            <div class="form-group">
+              <label for="passwordConfirm">再次確認密碼</label>
+              <input
+                type="password"
+                class="form-control"
+                id="passwordConfirm"
+                v-model="register.pwdcheck"
+              />
+            </div>
+            <div class="form-row">
+              <div class="form-group col-6">
+                <label for="inputCity">縣市</label>
+                <select id="inputCity" class="form-control" v-model="register.country">
+                  <option selected disabled>請選擇</option>
+                  <option>高雄市</option>
+                  <option>台北市</option>
+                </select>
+              </div>
+              <div class="form-group col-6">
+                <label for="inputState">區域</label>
+                <select id="inputState" class="form-control" v-model="register.area">
+                  <option selected disabled>請選擇</option>
+                  <option>三民區</option>
+                  <option>前鎮區</option>
+                </select>
+              </div>
+              <div class="form-group col-12">
+                <label for="address">地址</label>
+                <input type="address" class="form-control" id="address" v-model="register.address" />
+              </div>
+            </div>
+            <div class="form-group">
+              <label for="identify">特定寵物業許可證號</label>
+              <input type="text" class="form-control" id="identify" v-model="register.pblicense" />
+            </div>
+            <div class="form-group">
+              <label for="date">有效日期</label>
+              <input type="date" class="form-control" id="date" v-model="register.effectivedate" />
+            </div>
+
+            <div class="form-group d-flex">
+              <label for="upload" class="w-100 btn btn-dark">
+                廠商頭像上傳
+                <input type="file" id="upload" class="d-none" />
+              </label>
+            </div>
+            <div
+              class="rounded-circle overflow-hidden"
+              :style="{backgroundImage: 'url(' + preview + ')'}"
+              style="background-size: cover;"
+            >
+              <img
+                src="https://upload.cc/i1/2020/09/01/IaZYfp.png"
+                :class="{opacityZero:preview != null}"
+                class="w-100 img-fluid"
+                alt
+              />
+            </div>
+
+            <div class="form-group d-flex mt-4">
+              <router-link to="/Login" class="w-50 mr-2  btn btn-outline-primary">返回上一頁</router-link>
+              <button type="button" class="w-50 ml-2 btn btn-primary" @click="firmRegister">廠商註冊</button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
+
+<script>
+export default {
+  data () {
+    return {
+      register: {
+        companyname: '子庭',
+        companybrand: '子庭',
+        phone: '1',
+        email: '1',
+        pwd: '1',
+        pwdcheck: '1',
+        country: '高雄市',
+        area: '三民區',
+        address: '大昌路',
+        pblicense: '1',
+        effectivedate: '2020-08-01',
+        pic: null
+      },
+      preview: null
+    }
+  },
+  methods: {
+    firmRegister: function () {}
+
+  }
+}
+</script>
