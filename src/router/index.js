2470e7237211c2b9e66f1c89bb8e41c343b026a4
Zach <awdx8326@gmail.com>
2020-09-06 16:42:00 +0800

    組織 router 結構

--- src/router/index.js
+++ src/router/index.js
@@ -1,22 +1,82 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Layout from '../views/Layout.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
-    path: '/',
-    name: 'Home',
-    component: Home
+    path: '*',
+    redirect: '/'
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/',
+    component: Layout,
+    children:
+    [
+      {
+        path: '',
+        component: () => import('../views/Home.vue')
+      },
+      {
+        path: '/FirmList',
+        component: () => import('../views/FirmList.vue')
+      },
+      {
+        path: '/Login',
+        component: () => import('../views/Login.vue')
+      },
+      {
+        path: '/FirmRegister',
+        component: () => import('../views/FirmRegister.vue')
+      },
+      {
+        path: '/MemberRegister',
+        component: () => import('../views/MemberRegister.vue')
+      },
+      {
+        path: '/FirmId/:FirmId',
+        component: () => import('../views/FirmPage.vue')
+      },
+      {
+        path: '/FirmId/:FirmId/RoomId/:RoomId',
+        component: () => import('../views/Room.vue')
+      },
+      {
+        path: '/FirmBackstage',
+        component: () => import('../views/FirmBackstage.vue'),
+        children:
+        [
+          {
+            path: 'FirmOrder',
+            component: () => import('../views/FirmBackstage/FirmOrder.vue')
+          },
+          {
+            path: 'FirmRoom',
+            component: () => import('../views/FirmBackstage/FirmRoom.vue')
+          },
+          {
+            path: 'FirmSet',
+            component: () => import('../views/FirmBackstage/FirmSet.vue')
+          }
+        ]
+      },
+      {
+        path: '/MemberBackstage',
+        component: () => import('../views/MemberBackstage.vue'),
+        children:
+        [
+          {
+            path: 'MemberOrder',
+            component: () => import('../views/MemberBackstage/MemberOrder.vue')
+          },
+          {
+            path: 'MemberSet',
+            component: () => import('../views/MemberBackstage/MemberSet.vue')
+          }
+        ]
+      }
+    ]
   }
 ]
 
