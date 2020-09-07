107745ad24cea9de8c9748156b4c70783e9ee75a
Zach <awdx8326@gmail.com>
2020-09-07 10:02:42 +0800

    安裝 vue-axios npm

--- src/main.js
+++ src/main.js
@@ -2,10 +2,13 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import $ from 'jquery'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+
 import 'bootstrap'
 
 Vue.config.productionTip = false
-
+Vue.use(VueAxios, axios)
 window.$ = $
 new Vue({
   router,
