dbf7765ed848bad11c7296ba05a7352222e62b9f
Zach <awdx8326@gmail.com>
2020-09-07 23:24:32 +0800

    安裝 Vee-validate npm

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import router from './router'
 import $ from 'jquery'
 import axios from 'axios'
 import VueAxios from 'vue-axios'
+import { ValidationProvider, extend } from 'vee-validate'
 
 import 'bootstrap'
 
@@ -14,3 +15,10 @@ new Vue({
   router,
   render: h => h(App)
 }).$mount('#app')
+
+extend('secret', {
+  validate: value => value === 'example',
+  message: 'This is not the magic word'
+})
+
+Vue.component('ValidationProvider', ValidationProvider)
