ff3eec375e957e27317e4b9ebbdbc66470b49e7d
Zach <awdx8326@gmail.com>
2020-09-08 13:42:22 +0800

    修正 ValidationProvider 引入資料跟語系檔

--- src/main.js
+++ src/main.js
@@ -4,9 +4,12 @@ import router from './router'
 import $ from 'jquery'
 import axios from 'axios'
 import VueAxios from 'vue-axios'
-import { ValidationProvider, extend } from 'vee-validate'
-
+import { ValidationObserver, ValidationProvider, extend, localize, configure } from 'vee-validate'
+import * as rules from 'vee-validate/dist/rules'
+import { required, email } from 'vee-validate/dist/rules'
+import zhTW from 'vee-validate/dist/locale/zh_TW.json'
 import 'bootstrap'
+localize('zh_TW', zhTW)
 
 Vue.config.productionTip = false
 Vue.use(VueAxios, axios)
@@ -20,5 +23,33 @@ extend('secret', {
   validate: value => value === 'example',
   message: 'This is not the magic word'
 })
+Object.keys(rules).forEach(rule => {
+  extend(rule, {
+    ...rules[rule], // copies rule configuration
+  })
+})
+extend('required', {
+  ...required,
+  message: '此為必填'
+})
+extend('email', {
+  ...email,
+  message: '信箱格式錯誤'
+})
+extend('password', {
+  params: ['target'],
+  validate (value, { target }) {
+    return value === target
+  },
+  message: '密碼不相同'
+})
 
+configure({
+  classes: {
+    valid: 'is-valid',
+    invalid: 'is-invalid'
+  }
+})
+
+Vue.component('ValidationObserver', ValidationObserver)
 Vue.component('ValidationProvider', ValidationProvider)
