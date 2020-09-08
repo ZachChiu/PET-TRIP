d5bac0fb3d238728e03d21fdf91262cc1ff1a3ad
Zach <awdx8326@gmail.com>
2020-09-08 13:42:37 +0800

    製作登入驗證

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -43,67 +43,109 @@
               role="tabpanel"
               aria-labelledby="member-tab"
             >
-              <form class="py-5 px-2 px-sm-5" @submit.prevent="memberLogin">
-                <h3 class="text-center mb-3">會員登入</h3>
-                <div class="form-group">
-                  <label for="memberEmail">帳號</label>
-                  <input
-                    type="text"
-                    class="form-control"
-                    id="memberEmail"
-                    aria-describedby="emailHelp"
-                    placeholder="Enter email"
-                    v-model="login.email"
-                  />
-                </div>
-                <div class="form-group">
-                  <label for="memberPassword">密碼</label>
-                  <input
-                    type="password"
-                    class="form-control"
-                    id="memberPassword"
-                    placeholder="Password"
-                    v-model="login.password"
-                  />
-                  <a href="#">忘記密碼?</a>
-                </div>
+              <ValidationObserver v-slot="{ invalid }">
+                <form class="py-5 px-2 px-sm-5" @submit.prevent="memberLogin">
+                  <h3 class="text-center mb-3">會員登入</h3>
+                  <ValidationProvider rules="required|email" v-slot="{ errors,classes }">
+                    <div class="form-group">
+                      <label for="memberEmail">
+                        帳號
+                        <span class="text-danger">{{errors[0]}}</span>
+                      </label>
+                      <input
+                        type="text"
+                        class="form-control"
+                        id="memberEmail"
+                        aria-describedby="emailHelp"
+                        placeholder="Enter email"
+                        v-model="login.email"
+                        :class="classes"
+                      />
+                    </div>
+                  </ValidationProvider>
+                  <ValidationProvider rules="required" v-slot="{ errors,classes }">
+                    <div class="form-group">
+                      <label for="memberPassword">
+                        密碼
+                        <span class="text-danger">{{errors[0]}}</span>
+                      </label>
+                      <input
+                        type="password"
+                        class="form-control"
+                        id="memberPassword"
+                        placeholder="Password"
+                        v-model="login.password"
+                        :class="classes"
+                      />
+                      <a href="#">忘記密碼?</a>
+                    </div>
+                  </ValidationProvider>
 
-                <div class="form-group d-flex">
-                  <router-link to="/MemberRegister" class="w-50 mr-2 btn btn btn-outline-primary">會員註冊</router-link>
-                  <button type="submit" class="w-50 ml-2 btn btn-primary">會員登入</button>
-                </div>
-              </form>
+                  <div class="form-group d-flex">
+                    <router-link
+                      to="/MemberRegister"
+                      class="w-50 mr-2 btn btn btn-outline-primary"
+                    >會員註冊</router-link>
+                    <button
+                      type="submit"
+                      class="w-50 ml-2 btn btn-primary"
+                      :disabled="invalid"
+                      :class="{disabled:invalid}"
+                    >會員登入</button>
+                  </div>
+                </form>
+              </ValidationObserver>
             </div>
             <div class="tab-pane fade" id="firm" role="tabpanel" aria-labelledby="firm-tab">
-              <form class="py-5 px-2 px-sm-5" @submit.prevent="firmLogin">
-                <h3 class="text-center mb-3">廠商登入</h3>
-                <div class="form-group">
-                  <label for="firmEmail">帳號</label>
-                  <input
-                    type="text"
-                    class="form-control"
-                    id="firmEmail"
-                    aria-describedby="emailHelp"
-                    placeholder="Enter email"
-                    v-model="login.email"
-                  />
-                </div>
-                <div class="form-group">
-                  <label for="firmPassword">密碼</label>
-                  <input
-                    type="password"
-                    class="form-control"
-                    id="firmPassword"
-                    placeholder="Password"
-                    v-model="login.password"
-                  />
-                  <a href="#">忘記密碼?</a>
-                </div>
-                <div class="form-group d-flex">
-                  <router-link to="/FirmRegister" class="w-50 mr-2 btn btn btn-outline-primary">廠商註冊</router-link>
-                  <button type="submit" class="w-50 ml-2 btn btn-primary">廠商登入</button>
-                </div>
-              </form>
+              <ValidationObserver v-slot="{ invalid }">
+                <form class="py-5 px-2 px-sm-5" @submit.prevent="firmLogin">
+                  <h3 class="text-center mb-3">廠商登入</h3>
+                  <ValidationProvider rules="required|email" v-slot="{ errors,classes }">
+                    <div class="form-group">
+                      <label for="firmEmail">
+                        帳號
+                        <span class="text-danger">{{errors[0]}}</span>
+                      </label>
+                      <input
+                        type="text"
+                        class="form-control"
+                        id="firmEmail"
+                        aria-describedby="emailHelp"
+                        placeholder="Enter email"
+                        v-model="login.email"
+                        :class="classes"
+                      />
+                    </div>
+                  </ValidationProvider>
+
+                  <ValidationProvider rules="required" v-slot="{ errors,classes }">
+                    <div class="form-group">
+                      <label for="firmPassword">
+                        密碼
+                        <span class="text-danger">{{errors[0]}}</span>
+                      </label>
+                      <input
+                        type="password"
+                        class="form-control"
+                        id="firmPassword"
+                        placeholder="Password"
+                        v-model="login.password"
+                        :class="classes"
+                      />
+                      <a href="#">忘記密碼?</a>
+                    </div>
+                  </ValidationProvider>
+
+                  <div class="form-group d-flex">
+                    <router-link
+                      to="/FirmRegister"
+                      class="w-50 mr-2 btn btn btn-outline-primary"
+                    >廠商註冊</router-link>
+                    <button type="submit" class="w-50 ml-2 btn btn-primary" :disabled="invalid"
+                      :class="{disabled:invalid}">廠商登入</button>
+                  </div>
+                </form>
+              </ValidationObserver>
             </div>
           </div>
         </div>
@@ -138,7 +180,9 @@ export default {
           console.log(response)
           if (response.data.result === '登入成功') {
             const token = response.data.token
-            document.cookie = `pet=${token};expires=${new Date() * 1000}; path=/`
+            document.cookie = `pet=${token};expires=${
+              new Date() * 1000
+            }; path=/`
           }
         })
         .catch(function (error) {
@@ -160,14 +204,15 @@ export default {
           console.log(response)
           if (response.data.result === '登入成功') {
             const token = response.data.token
-            document.cookie = `pet=${token};expires=${new Date() * 1000}; path=/`
+            document.cookie = `pet=${token};expires=${
+              new Date() * 1000
+            }; path=/`
           }
         })
         .catch(function (error) {
           console.log(error)
         })
     }
-
   }
 }
 </script>
