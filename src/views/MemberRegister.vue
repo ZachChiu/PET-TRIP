0cb1da226ae0052e7924db7a2f353679e7b3ff4e
Zach <awdx8326@gmail.com>
2020-09-09 09:52:27 +0800

    會員註冊 新增驗證功能

--- src/views/MemberRegister.vue
+++ src/views/MemberRegister.vue
@@ -12,29 +12,80 @@
     <div class="container my-5">
       <div class="row">
         <div class="col-lg-6 col-11 mx-auto border rounded">
-          <form class="py-5 px-2 px-sm-5" @submit.prevent="memberRegister">
-            <h3 class="text-center mb-3">會員註冊</h3>
-            <div class="form-group">
-              <label for="name">姓名</label>
-              <input type="text" id="name" class="form-control" required v-model="register.membername"/>
-            </div>
-            <div class="form-group">
-              <label for="email">信箱(帳號)</label>
-              <input type="email" class="form-control" id="email" required v-model="register.email"/>
-            </div>
-            <div class="form-group">
-              <label for="password">密碼</label>
-              <input type="password" class="form-control" id="password" required v-model="register.pwd"/>
-            </div>
-            <div class="form-group">
-              <label for="passwordConfirm">再次確認密碼</label>
-              <input type="password" class="form-control" id="passwordConfirm" required v-model="register.pwdcheck"/>
-            </div>
-            <div class="form-group d-flex mt-4">
-              <router-link to="/Login" class="w-50 mr-2 btn btn-outline-primary">返回上一頁</router-link>
-              <button type="submit" class="w-50 ml-2 btn btn-primary">會員註冊</button>
-            </div>
-          </form>
+          <ValidationObserver v-slot="{ invalid }">
+            <form class="py-5 px-2 px-sm-5" @submit.prevent="memberRegister">
+              <h3 class="text-center mb-3">會員註冊</h3>
+              <ValidationProvider rules="required" v-slot="{ errors,classes }">
+                <div class="form-group">
+                  <label for="name">姓名</label>
+                  <input
+                    type="text"
+                    id="name"
+                    class="form-control"
+                    required
+                    v-model="register.membername"
+                    :class="classes"
+                  />
+                  <span class="text-danger">{{errors[0]}}</span>
+                </div>
+              </ValidationProvider>
+              <ValidationProvider rules="required|email" v-slot="{ errors,classes }">
+                <div class="form-group">
+                  <label for="email">信箱 (帳號)</label>
+                  <input
+                    type="email"
+                    class="form-control"
+                    id="email"
+                    required
+                    v-model="register.email"
+                    :class="classes"
+                  />
+                  <span class="text-danger">{{errors[0]}}</span>
+                </div>
+              </ValidationProvider>
+              <ValidationProvider name="密碼" rules="required|alpha_num" v-slot="{ errors,classes }">
+                <div class="form-group">
+                  <label for="password">密碼</label>
+                  <input
+                    type="password"
+                    class="form-control"
+                    id="password"
+                    required
+                    v-model="register.pwd"
+                    :class="classes"
+                  />
+                  <span class="text-danger">{{errors[0]}}</span>
+                </div>
+              </ValidationProvider>
+              <ValidationProvider
+                rules="required|password:@密碼|alpha_num"
+                v-slot="{ errors,classes }"
+              >
+                <div class="form-group">
+                  <label for="密碼">再次確認密碼</label>
+                  <input
+                    type="password"
+                    class="form-control"
+                    id="密碼"
+                    required
+                    v-model="register.pwdcheck"
+                    :class="classes"
+                  />
+                  <span class="text-danger">{{errors[0]}}</span>
+                </div>
+              </ValidationProvider>
+
+              <div class="form-group d-flex mt-4">
+                <router-link to="/Login" class="w-50 mr-2 btn btn-outline-primary">返回上一頁</router-link>
+                <button
+                  type="submit"
+                  class="w-50 ml-2 btn btn-primary"
+                  :disabled="invalid"
+                  :class="{disabled:invalid}"
+                >會員註冊</button>
+              </div>
+            </form>
+          </ValidationObserver>
         </div>
       </div>
     </div>
@@ -55,6 +106,7 @@ export default {
   },
   methods: {
     memberRegister: function () {
+      const vm = this
       const config = {
         method: 'post',
         url: 'http://pettrip.rocket-coding.com/api/Member/Register',
@@ -67,6 +119,9 @@ export default {
       this.$http(config)
         .then(function (response) {
           console.log(response)
+          if (response.data.result === '註冊成功') {
+            vm.$router.push('/Login')
+          }
         })
         .catch(function (error) {
           console.log(error)
