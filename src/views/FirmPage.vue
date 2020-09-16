087df7b3053274958c9de5455a0923560795a709
Zach <awdx8326@gmail.com>
2020-09-16 17:31:07 +0800

    將廠商個人頁面引入空間列表與評價列表的元件

--- src/views/FirmPage.vue
+++ src/views/FirmPage.vue
@@ -1,10 +1,12 @@
 <template>
-  <div class="firmPage" v-if="firmData.company != null">
+  <div class="firmPage">
+    <loading :active.sync="isLoading" loader="bars"></loading>
     <div class="banner">
       <div
         class="mx-auto"
         :style="{backgroundImage: 'url(' + firmData.company.bannerimg + ')'}"
         style="background-size: cover; background-position:center"
+        v-if="firmData.company != null"
       >
         <img
           src="https://upload.cc/i1/2020/09/09/GIaohq.jpg"
@@ -15,7 +17,7 @@
         />
       </div>
     </div>
-    <div class="container pt-3 position-relative" style="top:-70px">
+    <div class="container pt-3 position-relative" style="top:-70px" v-if="firmData.company != null">
       <div class="row mx-0 justify-content-center">
         <div class="py-3 bg-dark col-lg-5 col-md-6 col-sm-10">
           <div class="row justify-content-center text-light align-items-center">
@@ -42,7 +44,9 @@
                 <span v-if="firmData.company.pettype_cat && firmData.company.pettype_dog ">、</span>
                 <span v-if="firmData.company.pettype_dog">狗</span>
                 <span v-if="firmData.company.pettype_dog && firmData.company.pettype_other ">、</span>
-                <span v-if="firmData.company.pettype_cat && !firmData.company.pettype_dog && firmData.company.pettype_other ">、</span>
+                <span
+                  v-if="firmData.company.pettype_cat && !firmData.company.pettype_dog && firmData.company.pettype_other "
+                >、</span>
                 <span v-if="firmData.company.pettype_other">其他</span>
               </p>
             </div>
@@ -52,24 +56,37 @@
           class="py-3 col-lg-5 col-md-6 col-sm-10 bg-white border d-flex flex-column justify-content-center"
         >
           <div class="d-md-block d-none">
-            <div class="text-warning mb-0  d-flex align-items-end">
+            <div class="text-warning mb-0 d-flex align-items-end">
               <span class="text-dark mr-2">評價：{{firmData.company.evaluation_count}}筆</span>
-               <star-rating v-model="firmData.company.evaluation" :rounded-corners="true" :inline="true" :increment="0.1" :read-only="true" :star-size="22"></star-rating>
+              <star-rating
+                v-model="firmData.company.evaluation"
+                :rounded-corners="true"
+                :inline="true"
+                :increment="0.1"
+                :read-only="true"
+                :star-size="22"
+              ></star-rating>
             </div>
             <p v-if="!all" class="mb-0">
               回覆時段：
-               <span v-if="firmData.company.morning">早上</span>
-                <span v-if="firmData.company.morning && firmData.company.afternoon ">、</span>
-                <span v-if="firmData.company.morning && !firmData.company.afternoon && firmData.company.night">、</span>
-                <span v-if="firmData.company.morning && !firmData.company.afternoon && !firmData.company.night && firmData.company.midnight">、</span>
-                <span v-if="firmData.company.afternoon">下午</span>
-                <span v-if="firmData.company.afternoon && firmData.company.night ">、</span>
-                <span v-if="firmData.company.afternoon && !firmData.company.night && firmData.company.midnight ">、</span>
-                <span v-if="firmData.company.night">晚上</span>
-                <span v-if="firmData.company.night && firmData.company.midnight ">、</span>
-                <span v-if="firmData.company.midnight">半夜</span>
+              <span v-if="firmData.company.morning">早上</span>
+              <span v-if="firmData.company.morning && firmData.company.afternoon ">、</span>
+              <span
+                v-if="firmData.company.morning && !firmData.company.afternoon && firmData.company.night"
+              >、</span>
+              <span
+                v-if="firmData.company.morning && !firmData.company.afternoon && !firmData.company.night && firmData.company.midnight"
+              >、</span>
+              <span v-if="firmData.company.afternoon">下午</span>
+              <span v-if="firmData.company.afternoon && firmData.company.night ">、</span>
+              <span
+                v-if="firmData.company.afternoon && !firmData.company.night && firmData.company.midnight "
+              >、</span>
+              <span v-if="firmData.company.night">晚上</span>
+              <span v-if="firmData.company.night && firmData.company.midnight ">、</span>
+              <span v-if="firmData.company.midnight">半夜</span>
             </p>
-            <p v-if="all"  class="mb-0">回覆時段：24小時</p>
+            <p v-if="all" class="mb-0">回覆時段：24小時</p>
             <p class="mb-0">空間：{{firmData.company.Count}}間</p>
           </div>
           <div class="d-md-none d-flex text-center justify-content-around align-items-center">
@@ -80,18 +97,28 @@
             <div>|</div>
             <p v-if="!all" class="mb-0">
               回覆時段
-              <br />             <span v-if="firmData.company.morning">早上</span>
-                <span v-if="firmData.company.morning && firmData.company.afternoon ">、</span>
-                <span v-if="firmData.company.morning && !firmData.company.afternoon && firmData.company.night">、</span>
-                <span v-if="firmData.company.morning && !firmData.company.afternoon && !firmData.company.night && firmData.company.midnight">、</span>
-                <span v-if="firmData.company.afternoon">下午</span>
-                <span v-if="firmData.company.afternoon && firmData.company.night ">、</span>
-                <span v-if="firmData.company.afternoon && !firmData.company.night && firmData.company.midnight ">、</span>
-                <span v-if="firmData.company.night">晚上</span>
-                <span v-if="firmData.company.night && firmData.company.midnight ">、</span>
-                <span v-if="firmData.company.midnight">半夜</span>
+              <br />
+              <span v-if="firmData.company.morning">早上</span>
+              <span v-if="firmData.company.morning && firmData.company.afternoon ">、</span>
+              <span
+                v-if="firmData.company.morning && !firmData.company.afternoon && firmData.company.night"
+              >、</span>
+              <span
+                v-if="firmData.company.morning && !firmData.company.afternoon && !firmData.company.night && firmData.company.midnight"
+              >、</span>
+              <span v-if="firmData.company.afternoon">下午</span>
+              <span v-if="firmData.company.afternoon && firmData.company.night ">、</span>
+              <span
+                v-if="firmData.company.afternoon && !firmData.company.night && firmData.company.midnight "
+              >、</span>
+              <span v-if="firmData.company.night">晚上</span>
+              <span v-if="firmData.company.night && firmData.company.midnight ">、</span>
+              <span v-if="firmData.company.midnight">半夜</span>
             </p>
-            <p v-if="all"  class="mb-0">回覆時段 <br />24小時</p>
+            <p v-if="all" class="mb-0">
+              回覆時段
+              <br />24小時
+            </p>
             <div>|</div>
             <p class="mb-0">
               空間
@@ -112,56 +139,42 @@
       </div>
       <hr />
       <div class="container">
-        <div class="row justify-content-lg-start justify-content-md-between">
-          <div class="col-12 mb-3" v-for="(room,index) in firmData.roomlists" :key="index">
-            <div class="card mb-3">
-              <div class="row no-gutters">
-                <div class="col-md-4">
-                  <div
-                    class="h-100"
-                    :style="{backgroundImage: 'url(' + room.img1 + ')'}"
-                    style="background-size: cover;"
-                  >
-                    <img
-                      src="https://upload.cc/i1/2020/09/09/GIaohq.jpg"
-                      :class="{opacityZero:room.bannerimg != ''}"
-                      class="card-img"
-                      alt
-                    />
-                  </div>
-                </div>
-                <div class="col-md-8">
-                  <div class="card-body">
-                    <h5 class="card-title">
-                      <router-link
-                        :to="`/FirmPage/${room.companyseq}/Room/${room.roomseq}`"
-                        class="text-body stretched-link"
-                      >
-                        <i class="text-warning fas fa-star"></i>
-                        {{room.roomname}}
-                      </router-link>
-                    </h5>
-                    <p class="card-text">
-                      <i class="mr-1 fas fa-paw"></i>可接受：
-                             <span v-if="room.pettype_cat">貓</span>
-                <span v-if="room.pettype_cat && room.pettype_dog ">、</span>
-                <span v-if="room.pettype_dog">狗</span>
-                <span v-if="room.pettype_dog && room.pettype_other ">、</span>
-                <span v-if="room.pettype_cat && !room.pettype_dog && room.pettype_other">、</span>
-                <span v-if="room.pettype_other">其他</span>
-                    </p>
-                    <p class="card-text">
-                      <i class="mr-1 fas fa-sort-numeric-down"></i>可容納：{{room.roomamount}}隻
-                    </p>
-                    <p class="card-text">
-                      <i class="mr-1 fas fa-expand-alt"></i>接受重量：<span>{{room.petsizes}}</span><span v-if="room.petsizes != room.petsizee">~</span><span v-if="room.petsizes != room.petsizee">{{room.petsizee}}</span>公斤
-                    </p>
+        <ul class="nav nav-tabs text-center" id="myTab" role="tablist">
+          <li class="nav-item w-25" role="presentation">
+            <a
+              class="nav-link active"
+              id="space"
+              data-toggle="tab"
+              href="#nav-space"
+              role="tab"
+              aria-controls="nav-space"
+              aria-selected="true"
+            >空間</a>
+          </li>
+          <li class="nav-item w-25" role="presentation">
+            <a
+              class="nav-link border-top border-right bprder-left"
+              id="evaluation"
+              data-toggle="tab"
+              href="#nav-contact"
+              role="tab"
+              aria-controls="nav-contact"
+              aria-selected="false"
+            >評價</a>
+          </li>
+        </ul>
 
-                    <p class="card-text text-danger h5 text-right">$ {{room.roomprice}} / 天</p>
-                  </div>
-                </div>
-              </div>
-            </div>
+        <div class="tab-content" id="nav-tabContent">
+          <div
+            class="tab-pane fade show active bg-white border-top-0 border"
+            id="nav-space"
+            role="tabpanel"
+            aria-labelledby="space"
+          >
+            <firmPageRoom :firmData="firmData"></firmPageRoom>
+          </div>
+          <div class="tab-pane fade bg-white border-top-0 border" id="nav-contact" role="tabpanel" aria-labelledby="evaluation">
+            <firmPageEvaluation :firmData="firmData"></firmPageEvaluation>
           </div>
         </div>
       </div>
@@ -170,32 +183,59 @@
 </template>
 
 <script>
+/* global $ */
+import VueLoading from 'vue-loading-overlay'
+import 'vue-loading-overlay/dist/vue-loading.css'
+import firmPageRoom from '@/components/firmPageRoom.vue'
+import firmPageEvaluation from '@/components/firmPageEvaluation.vue'
+
 export default {
   data () {
     return {
       firmData: {},
       id: {},
-      all: false
+      all: false,
+      isLoading: false
     }
+  },
+  components: {
+    loading: VueLoading,
+    firmPageRoom,
+    firmPageEvaluation
   },
   created () {
     this.id = this.$route.params
     this.getData()
+    $('html, body').animate(
+      {
+        scrollTop: $('#app').offset().top
+      },
+      0
+    )
   },
   methods: {
     getData: function () {
+      const vm = this
+      this.isLoading = true
       const url = `http://pettrip.rocket-coding.com/api/Room/GetRoomslist?id=${this.id.FirmId}`
       this.$http
         .get(url)
         .then((res) => {
           console.log(res)
           this.firmData = res.data
-          if (this.firmData.company.morning && this.firmData.company.afternoon && this.firmData.company.night && this.firmData.company.midnight) {
+          if (
+            this.firmData.company.morning &&
+            this.firmData.company.afternoon &&
+            this.firmData.company.night &&
+            this.firmData.company.midnight
+          ) {
             this.all = true
           }
+          vm.isLoading = false
         })
         .catch((err) => {
           console.log(err)
+          vm.isLoading = false
         })
     }
   }
