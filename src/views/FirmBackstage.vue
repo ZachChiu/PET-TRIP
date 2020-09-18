95a3742a7870d5f7a59f6ad0bb8851cc79283afe
Zach <awdx8326@gmail.com>
2020-09-18 22:26:33 +0800

    調整會員後台 router-link 連到後台 QA 頁面

--- src/views/FirmBackstage.vue
+++ src/views/FirmBackstage.vue
@@ -9,67 +9,71 @@
         alt
       />
     </div>
-    <nav class="sideBar show bg-white position-absolute">
-      <a href="#" id="sidebarBtn" class="sidebarBtn btn btn-secondary rounded-0 position-absolute" @click.prevent="toggleSidebar">
-        <i class="change other fas fa-chevron-left d-none"></i>
-        <i class="change fas fa-chevron-right d-none"></i>
-      </a>
-      <h5 class="text-center my-4">廠商後台管理</h5>
-      <ul class="nav nav-pills nav-fill flex-column text-center">
-        <li class="nav-item" @click="pageCurrent = '/FirmBackstage'">
-          <router-link to="/FirmBackstage" class="nav-link" :class="{active:pageCurrent == '/FirmBackstage'}"><i class="fas fa-clipboard-list"></i>
-            訂單列表</router-link>
-        </li>
-        <li class="nav-item" @click="pageCurrent = '/FirmBackstage/FirmRoom'">
-            <router-link to="/FirmBackstage/FirmRoom" class="nav-link" :class="{active:pageCurrent == '/FirmBackstage/FirmRoom'}"><i class="fas fa-box-open"></i>空間管理</router-link>
-        </li>
-        <li class="nav-item" @click="pageCurrent = '/FirmBackstage/FirmQA'">
-            <router-link to="/FirmBackstage" class="nav-link" :class="{active:pageCurrent == '/FirmBackstage/FirmQA'}"><i class="fas fa-question"></i>問與答QA</router-link>
-        </li>
-        <li class="nav-item" @click="pageCurrent = '/FirmBackstage/FirmSet'">
-          <router-link to="/FirmBackstage/FirmSet" class="nav-link" :class="{active:pageCurrent == '/FirmBackstage/FirmSet'}"><i class="fas fa-cog"></i>廠商設定</router-link>
-        </li>
-      </ul>
-    </nav>
-    <router-view @checkStatus="getFirmBackstageData"></router-view>
+    <div class="container py-4">
+      <div class="row">
+        <div class="col-md-3 col-12 mb-3">
+          <nav class="bg-light">
+            <ul class="nav nav-pills flex-md-column text-center">
+              <li class="nav-item" @click="pageCurrent = '/FirmBackstage'">
+                <router-link
+                  to="/FirmBackstage"
+                  class="nav-link"
+                  :class="{active:pageCurrent == '/FirmBackstage'}"
+                >
+                  <i class="fas fa-clipboard-list"></i>
+                  訂單列表
+                </router-link>
+              </li>
+              <li class="nav-item" @click="pageCurrent = '/FirmBackstage/FirmRoom'">
+                <router-link
+                  to="/FirmBackstage/FirmRoom"
+                  class="nav-link"
+                  :class="{active:pageCurrent == '/FirmBackstage/FirmRoom'}"
+                >
+                  <i class="fas fa-box-open"></i>空間管理
+                </router-link>
+              </li>
+              <li class="nav-item" @click="pageCurrent = '/FirmBackstage/FirmQA'">
+                <router-link
+                  to="/FirmBackstage/FirmQA"
+                  class="nav-link"
+                  :class="{active:pageCurrent == '/FirmBackstage/FirmQA'}"
+                >
+                  <i class="fas fa-question"></i>問與答QA
+                </router-link>
+              </li>
+              <li class="nav-item" @click="pageCurrent = '/FirmBackstage/FirmSet'">
+                <router-link
+                  to="/FirmBackstage/FirmSet"
+                  class="nav-link"
+                  :class="{active:pageCurrent == '/FirmBackstage/FirmSet'}"
+                >
+                  <i class="fas fa-cog"></i>廠商設定
+                </router-link>
+              </li>
+            </ul>
+          </nav>
+        </div>
+        <div class="col-md-9 col-12">
+          <router-view :identify="identify" @checkStatus="getFirmBackstageData"></router-view>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
-<style lang="scss">
-.sidebarBtn {
-  top: 5px;
-  left: 100%;
-  .other {
-    display: block !important;
-  }
-}
-.sideBar {
-  z-index: 3;
-  transition: all 1s;
-  left: -180px;
-  width: 180px;
-  &.show {
-    left: 0;
-  }
-}
-</style>
-
 <script>
-/* global $ */
 export default {
   data () {
     return {
       pageCurrent: '/FirmBackstage'
     }
   },
+  props: ['identify'],
   created () {
     this.getFirmBackstageData()
   },
   methods: {
-    toggleSidebar: function () {
-      $('.sideBar').toggleClass('show')
-      $('.change').toggleClass('other')
-    },
     getFirmBackstageData: function () {
       this.pageCurrent = this.$route.path
       const token = document.cookie.replace(
